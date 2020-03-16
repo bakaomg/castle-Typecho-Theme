a198e9fb0ad011bb90d3370685b7d3e6985898db
ohmyga <40896676+ohmyga233@users.noreply.github.com>
2020-03-16 09:34:17 +0800

    🤔 默认暗色还是不交给 js 处理了，免得眼瞎（

--- core/inc/header.php
+++ core/inc/header.php
@@ -19,7 +19,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
  </head>
 
- <body class="mdui-theme-primary-<?php $this->options->themeColor(); ?> mdui-theme-accent-<?php $this->options->themeAccentColor(); ?>">
+ <body class="mdui-theme-primary-<?php $this->options->themeColor(); ?> mdui-theme-accent-<?php $this->options->themeAccentColor(); ?><?php echo (Helper::options()->themeDarkColor == 'dark') ? ' mdui-theme-layout-dark' : NULL; ?>">
 
   <header class="mdui-appbar mdui-appbar-fixed" id="header"></header>
 
