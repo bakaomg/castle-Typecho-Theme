56dfb5fd7702ad662df3e1c2e7c8ce555e091ca2
ohmyga <40896676+bakaomg@users.noreply.github.com>
2020-11-07 20:18:35 +0800

    😛 将旧版本迁移到 0.9.x 分支

--- core/inc/toc.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Castle Toc Sidebar
- * Last Update: 2020/03/17
- */
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-?>
-<div class="mdui-shadow-3<?php echo (Castle_Footer::tocTransparent()) ? Castle_Footer::tocTransparent() : NULL; ?>" id="toc-sidebar"></div>
